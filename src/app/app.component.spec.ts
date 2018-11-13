a5ec142dd43bf293c491ed547f88f5c93bd133ec
Gene Connolly <geneconnolly@mw-gene.local>
2018-11-12 21:26:44 -0500

    rename to unpkg-immutable-example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-immutable-example'`, () => {
+  it(`should have as title 'unpkg-immutable-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-immutable-example');
+    expect(app.title).toEqual('unpkg-immutable-example');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-immutable-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to unpkg-immutable-example!');
   });
 });
