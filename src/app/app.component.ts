29d215f31a8d6a98cbb6c873d08d03715bddc5e5
Gene Connolly <geneconnolly@mw-gene.local>
2018-11-25 22:32:48 -0500

    start to add content describing the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'unpkg-immutable-example';
+  color = environment.color;
 }
